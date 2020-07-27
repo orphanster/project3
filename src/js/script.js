c789b59fae39a463c778f525493c698215b98ed1
Bohdan <bogdansyrota@gmail.com>
2020-07-27 03:29:14 +0300

    catalog added

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev.svg"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"</button>',
+      });
+  });
